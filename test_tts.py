dbd3e4e82f2022c430eca2c7f53d2439031c3038
TaehyeonJeon <taehyun0540@gmail.com>
2023-04-02 20:25:08 +0900

    test

--- test_tts.py
+++ test_tts.py
@@ -2,14 +2,12 @@ from gtts import gTTS
 import playsound as ps
 import os
 
-if __name__ == '__main__':
-    tts = gTTS(
-        text='안녕하세요',
-        lang='ko', slow=False
-    )
-    tts.save('ex_ko.mp3')
-    #음원 재생
-    ps.playsound('ex_ko.mp3')
-    #재생한 음원 삭제
-    os.remove('ex_en.mp3')
-    os.remove('ex_ko.mp3')
+tts = gTTS(
+    text='안녕하세요',
+    lang='ko', slow=False
+)
+tts.save('ex_ko.mp3')
+#음원 재생
+ps.playsound('ex_ko.mp3')
+#재생한 음원 삭제
+os.remove('ex_ko.mp3')
