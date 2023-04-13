c63406026653dd001710c1988ab5b146e17d7d2e
TaehyeonJeon <taehyun0540@gmail.com>
2023-04-13 18:09:11 +0900

    .

--- test_tts.py
+++ test_tts.py
@@ -2,12 +2,9 @@ from gtts import gTTS
 import playsound as ps
 import os
 
-tts = gTTS(
-    text='안녕하세요',
-    lang='ko', slow=False
-)
-tts.save('ex_ko.mp3')
-#음원 재생
-ps.playsound('ex_ko.mp3')
-#재생한 음원 삭제
-os.remove('ex_ko.mp3')
+tts = gTTS(text="안녕하세요", lang="ko", slow=False)
+tts.save("ex_ko.mp3")
+# 음원 재생
+ps.playsound("ex_ko.mp3")
+# 재생한 음원 삭제
+os.remove("ex_ko.mp3")
